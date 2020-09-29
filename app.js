07cd0edbdcdabdd1a666c9b08efea27d56e0a12b
mennaashraff7 <39194503+mennaashraff7@users.noreply.github.com>
2020-09-29 21:25:56 +0200

    updated js

--- app.js
+++ app.js
@@ -3,7 +3,8 @@
 const status = document.querySelector(`.status`);
 const reset = document.querySelector(`.reset-btn`);
 const cells =  document.querySelectorAll(`.game-cell`);
-    var winner=null;
+const winnerAlert = document.getElementById(`winner-alert`);
+    var winner = "null";
     var x=true; 
     var live=true;
 
@@ -14,11 +15,13 @@ function solve(){
        return;
    }else if(x){
         if(this.classList[2] !== "x" && this.classList[2] !== "o" ){
+            document.getElementById("status").innerHTML =" o is next";
             x=!x;
       this.classList.add("x");
         }
     }else{
         if(this.classList[2] !== "x" && this.classList[2] !== "o"){
+            document.getElementById("status").innerHTML =" x is next";
             x=!x;
       this.classList.add("o");  
         }
@@ -29,7 +32,9 @@ function solve(){
 
 function res(){
     x=true;
-    winner=null;
+    document.getElementById("status").innerHTML ="x is next";
+    document.getElementById("tied").innerHTML ="congratulations";
+    winnerAlert.classList.remove("active");
     for(const cell of cells){
         if(cell.classList[2] === "x"){
     cell.classList.remove('x');
@@ -43,8 +48,8 @@ function res(){
     }
     live = true;
 }
-reset.addEventListener('click', res);
 cells.forEach(cell => cell.addEventListener('click', solve));
+reset.addEventListener('click', res);
 
 
 function winTest(){
@@ -61,23 +66,59 @@ const bottomRight = cells[8].classList[2];
     console.log(topLeft,topMiddle,topRight);
     if(topLeft && topLeft === topMiddle && topLeft === topRight){
         live=false;
+        winner=topLeft;
+        document.getElementById("status").innerHTML =  winner + " is the winner";
+        document.getElementById("winner").innerHTML = winner + " won!";
+        winnerAlert.classList.add("active");
        }else if(topLeft && topLeft === middleLeft && topLeft === bottomLeft){
                 live=false;
-                
+                winner=topLeft;
+                document.getElementById("status").innerHTML =  winner + " is the winner";
+           document.getElementById("winner").innerHTML = winner + " won!";
+           winnerAlert.classList.add("active");
         }else if(topLeft && topLeft === middleMiddle && topLeft === bottomRight){
                 live=false;
-        }else if(topLeft && topLeft === middleMiddle && topLeft === bottomRight){
-                live=false;
+                winner=topLeft;
+            document.getElementById("status").innerHTML =  winner + " is the winner";
+            document.getElementById("winner").innerHTML = winner + " won!";
+            winnerAlert.classList.add("active");
         }else if(middleLeft && middleLeft === middleMiddle && middleLeft === middleRight){
                 live=false;
+                winner=middleLeft;
+            document.getElementById("status").innerHTML =  winner + " is the winner";
+            document.getElementById("winner").innerHTML = winner + " won!";
+            winnerAlert.classList.add("active");
         }else if(bottomLeft && bottomLeft === bottomMiddle && bottomLeft === bottomRight){
                 live=false;
+            winner=bottomLeft;
+            document.getElementById("status").innerHTML =  winner + " is the winner";
+            document.getElementById("winner").innerHTML = winner + " won!";
+            winnerAlert.classList.add("active");
         }else if(topMiddle && topMiddle === middleMiddle && topMiddle === bottomMiddle){
                 live=false;
+            winner=topMiddle;
+            document.getElementById("status").innerHTML =  winner + " is the winner";
+            document.getElementById("winner").innerHTML = winner + " won!";
+            winnerAlert.classList.add("active");
         }else if(topRight && topRight === middleMiddle && topRight === bottomLeft){
                 live=false;
+                winner=topRight;
+            document.getElementById("status").innerHTML =  winner + " is the winner";
+            document.getElementById("winner").innerHTML = winner + " won!";
+            winnerAlert.classList.add("active");
         }else if(topRight && topRight === middleRight && topRight === bottomRight){
                 live=false;
-        }
+           winner=topRight;
+            document.getElementById("status").innerHTML =  winner + " is the winner";
+            document.getElementById("winner").innerHTML = winner + " won!";
+            winnerAlert.classList.add("active");
+        }else if( topLeft &&
+ topMiddle && topRight && middleLeft &&middleMiddle && middleRight && bottomLeft && bottomMiddle && bottomRight){
+                 document.getElementById("status").innerHTML = "game is tied!";
+            document.getElementById("tied").innerHTML ="Ooops!";
+            document.getElementById("tied").style.color = "red";
+            document.getElementById("winner").innerHTML ="game is tied!";
+            winnerAlert.classList.add("active");
+            }
     
     }
